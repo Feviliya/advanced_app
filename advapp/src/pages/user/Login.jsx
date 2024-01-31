5e4157bc86f02ba6857d4cf84f0568e462431d6f
Feviliya J <93245596+Feviliya@users.noreply.github.com>
2024-01-31 10:42:48 +0530

    Delete advapp directory

--- advapp/src/pages/user/Login.jsx
+++
@@ -1,9 +0,0 @@
-import { Loginbox } from "../../components/user/Loginbox"
-import '../../assets/css/user/Login.css'
-export const Login = () => {
-  return (
-    <div id="loginbox">
-        <Loginbox></Loginbox>
-    </div>
-  )
-}
